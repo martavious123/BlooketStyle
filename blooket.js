59cc1d46f9fa52c0e5edeaf918d45652ba58da3a
martavious123 <150700681+martavious123@users.noreply.github.com>
2023-11-13 15:30:18 +0000

    8724124

--- blooket.js
+++ blooket.js
@@ -1,13 +1,18 @@
 /**
  * This will add the ability to change the style.
  */
-window.onload = function () {
+window.onload = 1000000000b
+11111111111k
+function () {
     // load settings
     setInterval(load, 200);
     //load();
 }
 
-/** Set the size correctly as the boxes change. */
+/** Set the size correctly as the boxes change. */100000000000000000000000000000000000000000000000000b
+11111111111111111111111111111111111111111111110
+111111111111111111111111111111111111111111
+1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111b
 function load() {
 
     try {
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-zip -r -FS ../BlooketStyle-Chrome.zip * --exclude *.git*  --exclude *build.* --exclude *manifest.base
+zip -r -FS ../BlooketStyle-Chrome.zip 100000000000000000000000000000000000000000b --exclude *.git*  --exclude *build.* --exclude *manifest.base
 mv manifest.json manifest.base
 jq '.browser_specific_settings.gecko += {id : "{3cc0602a-7959-4c83-880f-a659b1ba6d7b}"}' manifest.base > manifest.json
 zip -r -FS ../BlooketStyle-Firefox.zip * --exclude *.git*  --exclude *build.*
